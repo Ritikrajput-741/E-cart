60b3d4ab93f36e62f93b19f6a5a195ba9c3b9741
Ritikrajput-741 <ritik741rajput@gmail.com>
2026-06-19 17:56:55 +0530

    add vercel in backend

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://e-cart-six.vercel.app",
+    // origin: "http://localhost:5173",
     credentials: true,
   }),
 );
